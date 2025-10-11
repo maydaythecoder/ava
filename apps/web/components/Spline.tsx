ff21f43dbd8119bf2b89fce5ef02de85ffbc7985
mayday <maydaythecoder@gmail.com>
2025-10-10 21:42:26 -0400

    Refactor Home component to integrate Spline 3D visualization; remove placeholder components and streamline layout for improved user experience.

---
+++ apps/web/components/Spline.tsx
@@ -0,0 +1,13 @@
+import Spline from '@splinetool/react-spline/next';
+
+const SplineComponent = () => {
+  return (
+    <main className='w-full h-[100vh]'>
+      <Spline
+        scene="https://prod.spline.design/w0Bh6v-nKPBHO2hH/scene.splinecode" 
+      />
+    </main>
+  )
+}
+
+export default SplineComponent
